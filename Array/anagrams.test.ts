5b4576de20a9e570e0f762dcd8833903a5e6edd1
frivolta <rivoltafilippo@gmail.com>
2021-01-18 23:00:49 +0100

    Anagrams

--- Array/anagrams.test.ts
+++ Array/anagrams.test.ts
@@ -1,42 +1,66 @@
-import { groupAnagrams, isAnagram } from './anagrams';
+import {
+  groupAnagrams,
+  groupAnagramsMap,
+  groupAnagramsReduce,
+} from "./anagrams";
 
-describe('func: isAnagram', () => {
-  it('returns true for anagram input', () => {
-    const input = "eat"
-    const checkValue="tea"
-    expect(isAnagram(input, checkValue)).toBeTruthy()
+describe("Method 1.", () => {
+  it("works with regular inputs", () => {
+    const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+    const output = [["ate", "eat", "tea"], ["bat"], ["nat", "tan"]];
+    const result = groupAnagrams(strs);
+    expect(result).toEqual(output);
   });
-  it('returns false for not anagram input', () => {
-    const input = "eat"
-    const checkValue="tab"
-    expect(isAnagram(input, checkValue)).toBeFalsy()
+  it("works with empty inputs", () => {
+    const strs = [""];
+    const output = [[""]];
+    const result = groupAnagrams(strs);
+    expect(result).toEqual(output);
   });
-  it('returns true for two empty strings', () => {
-    const input = ""
-    const checkValue=""
-    expect(isAnagram(input, checkValue)).toBeTruthy()
+  it("works with single char input", () => {
+    const strs = ["a"];
+    const output = [["a"]];
+    const result = groupAnagrams(strs);
+    expect(result).toEqual(output);
   });
 });
-
-
-describe('Method 1.', () => {
-  it('works with regular inputs', () => {
-    const strs = ["eat","tea","tan","ate","nat","bat"]
-    const output = [["eat","tea","ate"],["tan", "nat"], ["bat"]]
-    const result = groupAnagrams(strs)
-    expect(result).toEqual(output)
+describe("Method 2.", () => {
+  it("works with regular inputs", () => {
+    const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+    const output = [["ate", "eat", "tea"], ["bat"], ["nat", "tan"]];
+    const result = groupAnagramsMap(strs);
+    expect(result).toEqual(output);
   });
-  it('works with empty inputs', () => {
-    const strs = [""]
-    const output = [[""]]
-    const result = groupAnagrams(strs)
-    expect(result).toEqual(output)
+  it("works with empty inputs", () => {
+    const strs = [""];
+    const output = [[""]];
+    const result = groupAnagramsMap(strs);
+    expect(result).toEqual(output);
   });
-  it('works with single char input', () => {
-    const strs = ["a"]
-    const output = [["a"]]
-    const result = groupAnagrams(strs)
-    expect(result).toEqual(output)
+  it("works with single char input", () => {
+    const strs = ["a"];
+    const output = [["a"]];
+    const result = groupAnagramsMap(strs);
+    expect(result).toEqual(output);
   });
 });
- 
+describe("Method 3.", () => {
+  it("works with regular inputs", () => {
+    const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+    const output = [["ate", "eat", "tea"], ["bat"], ["nat", "tan"]];
+    const result = groupAnagramsReduce(strs);
+    expect(result).toEqual(output);
+  });
+  it("works with empty inputs", () => {
+    const strs = [""];
+    const output = [[""]];
+    const result = groupAnagramsReduce(strs);
+    expect(result).toEqual(output);
+  });
+  it("works with single char input", () => {
+    const strs = ["a"];
+    const output = [["a"]];
+    const result = groupAnagramsReduce(strs);
+    expect(result).toEqual(output);
+  });
+});
--- Array/anagrams.ts
+++ Array/anagrams.ts
@@ -20,26 +20,58 @@ Output: [["a"]] */
 /* function groupAnagrams(strs: string[]): string[][] {
   
 }; */
-const groupAnagrams = (strs: string[]): string[][]=> {
-  if(strs.length>10000)
-    throw "Exceeded strs length"
-  const groupedAnagrams = strs.reduce<string[][]>((groupedAcc, stringItem)=>{
-    
-  const singleGroupedAnagrams = strs.reduce<string[]>((acc, stringToCheck)=>{
-      return isAnagram(stringToCheck, stringItem) ? [...acc, stringToCheck] : acc
-    }, [])
-    return singleGroupedAnagrams.length && groupedAcc.filter(itemArray=>itemArray.toString()===singleGroupedAnagrams.toString()).length === 0  ? [...groupedAcc, singleGroupedAnagrams] : groupedAcc
-  }, [])
-  
-  if(groupedAnagrams.length)
-    return groupedAnagrams
-  throw "No anagram found"
-};
-const isAnagram = (value: string, checkValue: string): boolean =>{
-  const isSameLenght = value.length === checkValue.length;
-  const sortedValue = value.split("").sort().join();
-  const sortedCheckValue = checkValue.split("").sort().join()
-  return isSameLenght && sortedCheckValue === sortedValue
-}
 
-export { isAnagram, groupAnagrams}
+// Method 1. Create an object with sorted keys
+// Runtime: 132 ms :) Your runtime beats 74.32 % of typescript submissions.
+interface AnagramObject {
+  [key: string]: string[];
+}
+const groupAnagrams = (strs: string[]): string[][] => {
+  if (strs.length === 1) return [strs];
+  const anagramsObject: AnagramObject = {};
+  const sortedStrs = strs.sort();
+  sortedStrs.forEach((sortedStr) => {
+    const sortedString = sortedStr.split("").sort().join("");
+    anagramsObject.hasOwnProperty(sortedString)
+      ? (anagramsObject[sortedString] = [
+          ...anagramsObject[sortedString],
+          sortedStr,
+        ])
+      : (anagramsObject[sortedString] = [sortedStr]);
+  });
+  return Object.keys(anagramsObject).map((key) => anagramsObject[key]);
+};
+
+// Method 2. Use a Map instead of Object
+// Runtime: 272 ms
+const groupAnagramsMap = (strs: string[]): string[][] => {
+  if (strs.length === 1) return [strs];
+  const anagramsMap = new Map<string, string[]>();
+  const sortedStrs = strs.sort();
+  sortedStrs.forEach((sortedStr) => {
+    const sortedString = sortedStr.split("").sort().join();
+    const anagram = anagramsMap.get(sortedString);
+    anagramsMap.set(
+      sortedString,
+      anagram ? [...anagram, sortedStr] : [sortedStr]
+    );
+  });
+  return Array.from(anagramsMap.values());
+};
+
+// Method 3. Use reduce instead of foreach
+// More elegan but slow
+
+const groupAnagramsReduce = (strs: string[]): string[][] => {
+  if (strs.length === 1) return [strs];
+  const sortedStrs = strs.sort();
+  const anagramsObject = sortedStrs.reduce<AnagramObject>((acc, sortedStr) => {
+    const sortedString = sortedStr.split("").sort().join("");
+    return acc.hasOwnProperty(sortedString)
+      ? { ...acc, [sortedString]: [...acc[sortedString], sortedStr] }
+      : { ...acc, [sortedString]: [sortedStr] };
+  }, {});
+  return Object.keys(anagramsObject).map((key) => anagramsObject[key]);
+};
+
+export { groupAnagrams, groupAnagramsMap, groupAnagramsReduce };
