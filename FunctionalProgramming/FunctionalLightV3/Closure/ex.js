868a14f2df7db86437dfde29b5f15094ef460b02
Filippo Rivolta Mac <frivolta@MBPdiFilippo2.lan>
2021-06-22 10:22:29 +0200

    recursion

---
+++ FunctionalProgramming/FunctionalLightV3/Closure/ex.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function strBuilder(str) {
+  return function next(arg) {
+    return typeof arg === "string" ? strBuilder(str + arg) : str;
+  };
+}
+
+var hello = strBuilder("Hello, ");
+var kyle = hello("Kyle");
+var susan = hello("Susan");
+var question = kyle("?")();
+var greeting = susan("!")();
+
+console.log(strBuilder("Hello, ")("")("Kyle")(".")("")() === "Hello, Kyle.");
+console.log(hello() === "Hello, ");
+console.log(kyle() === "Hello, Kyle");
+console.log(susan() === "Hello, Susan");
+console.log(question === "Hello, Kyle?");
+console.log(greeting === "Hello, Susan!");
---
+++ FunctionalProgramming/FunctionalLightV3/Recursion/ex.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const strSubstr = (str) => str.substr(1, str.length - 2);
+const lettersAreEqual = (str) => str[0] === str[str.length - 1];
+
+function isPalindrome(str) {
+  if (str.length < 2) return true;
+  return lettersAreEqual(str) ? isPalindrome(strSubstr(str)) : false;
+}
+
+console.log(isPalindrome("") === true);
+console.log(isPalindrome("a") === true);
+console.log(isPalindrome("aa") === true);
+console.log(isPalindrome("aba") === true);
+console.log(isPalindrome("abba") === true);
+console.log(isPalindrome("abccba") === true);
+
+console.log(isPalindrome("ab") === false);
+console.log(isPalindrome("abc") === false);
+console.log(isPalindrome("abca") === false);
+console.log(isPalindrome("abcdba") === false);
